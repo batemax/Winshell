7eb3c9dc81fbb6f080a24346db4e317268a0a14e
bertranwang <bertranwang@gmail.com>
2018-01-21 11:44:27 +0800

    添加docs

---
+++ include/main.h
@@ -0,0 +1,12 @@
+//
+// Created by beauw on 2018/1/21.
+//
+
+#ifndef WINSHELL_MAIN_H
+#define WINSHELL_MAIN_H
+
+
+
+
+
+#endif //WINSHELL_MAIN_H
---
+++ src/test.cpp
@@ -0,0 +1,15 @@
+//
+// Created by beauw on 2018/1/20.
+// 测试是否可以运行
+//
+
+#include "iostream"
+using namespace std;
+// 测试
+//
+//int main(){
+//    cout<<"hello world!"<<endl;
+//
+//    return 0;
+//}
+
